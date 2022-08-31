14526eba060e1a797f237ef22872d5afab546bee
JinWoong Kim <em221@naver.com>
2022-08-31 19:56:04 +0900

    Initial commit

--- src/main/java/com/jw/mycaferecipe/repository/board/BoardRepository.java
+++ src/main/java/com/jw/mycaferecipe/repository/board/BoardRepository.java
@@ -1,7 +1,8 @@
 package com.jw.mycaferecipe.repository.board;
 
+import com.jw.mycaferecipe.entity.BoardDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository<> {
+public interface BoardRepository extends JpaRepository<BoardDTO, Long> {
 
 }
