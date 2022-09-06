096fe58d60d04380e2eeaf1740b775f6625ed3ec
JinWoong Kim <em221@naver.com>
2022-09-06 14:16:14 +0900

    Initial commit

--- src/main/java/com/jw/mycaferecipe/controller/HomeController.java
+++ src/main/java/com/jw/mycaferecipe/controller/HomeController.java
@@ -1,13 +1,36 @@
 package com.jw.mycaferecipe.controller;
 
+import com.jw.mycaferecipe.entity.MenuDTO;
+import com.jw.mycaferecipe.service.menu.MenuService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private final MenuService menuService;
+
+    public HomeController(MenuService menuService) {
+        this.menuService = menuService;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        MenuDTO beverage = menuService.menuList().stream()
+                .filter(s -> s.getSort().equals("beverage"))
+                .sorted()
+                .findFirst()
+                .orElse(null);
+
+
+        MenuDTO food = menuService.menuList().stream()
+                .filter(s -> s.getSort().equals("food"))
+                .findAny()
+                .orElse(null);
+
+        model.addAttribute("beverage", beverage);
+        model.addAttribute("food", food);
         return "home";
     }
 
